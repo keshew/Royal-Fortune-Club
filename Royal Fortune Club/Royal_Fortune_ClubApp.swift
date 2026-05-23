f72dff1160b13d7bdf581258abb7a27a9cbd6e77
keshew <artyom.korotckov2017@yandex.ru>
2026-05-23 06:13:44 +0500

    Initial Commit

---
+++ Royal Fortune Club/Royal_Fortune_ClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Royal_Fortune_ClubApp.swift
+//  Royal Fortune Club
+//
+//  Created by Артём Коротков on 23.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct Royal_Fortune_ClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
