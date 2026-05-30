34f1d2911465038a852da283b6fd8cf88abb9bbd
keshew <artyom.korotckov2017@yandex.ru>
2026-05-30 17:06:12 +0500

    fix

--- Royal Fortune Club/Royal_Fortune_ClubApp.swift
+++ Royal Fortune Club/Royal_Fortune_ClubApp.swift
@@ -2,9 +2,11 @@ import SwiftUI
 
 @main
 struct Royal_Fortune_ClubApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootContentView()
         }
     }
 }
